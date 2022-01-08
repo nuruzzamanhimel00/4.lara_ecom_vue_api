6b5d57b20213e6f386a4b76349138a7a672040fd
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-01-08 23:34:54 +0600

    edit product

--- app/Http/Controllers/Admin/Api/ProductController.php
+++ app/Http/Controllers/Admin/Api/ProductController.php
@@ -11,6 +11,8 @@ use App\ProductStatus;
 use DB;
 use Image;
 use App\Library\AcccessToken;
+use Hamcrest\Arrays\IsArray;
+use File;
 
 class ProductController extends Controller
 {
@@ -129,7 +131,89 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // return response()->json([
+        //    $request->all()
+        // ]);
+        $id = $request->id;
+        $prodcut_img_toArray_db = ProductImage::where('product_id',$request->id)
+        ->pluck('image')->toArray();
+        $form_product_imgArray_file = $request->input('files');
+
+        $diff_array = array_diff($prodcut_img_toArray_db,$form_product_imgArray_file);
+
+        // $array_diff_to_find_oldInmg_forRemove = array_diff($prodcut_img_toArray_db,$form_product_imgArray_file);
+        // $array_intersect_to_find_newImg_forStore = array_intersect($form_product_imgArray_file,$prodcut_img_toArray_db);
+
+
+        // return response()->json([
+        //      $diff_array
+        // ]);
+
+        if(empty($diff_array)){
+            $product_create_get_id = DB::table('products')->where('id',$id)->update([
+                "userid" => $request->userid,
+                "categoryId" => $request->categoryId,
+                "brandId" => $request->brandId,
+                "title" => $request->title,
+                "description" => $request->description,
+                "slug" => str_slug($request->title) ,
+                "quantity" => $request->quantity,
+                "price" => $request->price,
+                "offer_price" => isset($request->offer_price)? $request->offer_price : NULL,
+                "productstatus_id" => $request->productstatus_id,
+                'ip_address' => \Request::ip()
+            ]);
+
+            return response()->json([
+                'status' => 'success'
+            ]);
+        }else{
+            $emptyArray = [];
+            foreach($diff_array as $da){
+                if(File::exists(public_path('image/admin/products/product_items/'.$da))) {
+                    File::delete(public_path('image/admin/products/product_items/'.$da));
+                }
+                $productImageDelete = ProductImage::where('product_id',$request->id)
+                ->where('image',$da)->delete();
+                $emptyArray[] = $da;
+
+            }
+
+            foreach($form_product_imgArray_file as $file){
+                if (strpos($file, 'data') !== false) {
+                    $image = $file;
+                    // data:image/png;
+                    $strpos = strpos($image,';');
+                    $substr = substr($image,0,$strpos);
+                    $image_ext = substr(strrchr($substr,'/'),1);
+
+                    $imagename = str_slug($request->title)."_".rand().".".$image_ext;
+                    $image_save = public_path('image/admin/products/product_items/'.$imagename);
+
+                     // open an image file
+                    $img = Image::make($image);
+
+                    // save image in desired format
+                    if($img->save($image_save)){
+                        $productImage = ProductImage::create([
+                            "product_id" => $id,
+                            "image" => $imagename
+                        ]);
+                    }
+
+
+                }
+            }
+            return response()->json([
+                'status' => 'success'
+            ]);
+        }
+    //     return response()->json([
+    //         $emptyArray
+    //    ]);
+
+
+        // dd();
     }
 
     /**
@@ -140,6 +224,6 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+
     }
 }
